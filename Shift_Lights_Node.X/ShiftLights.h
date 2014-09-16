53087e1377481c46e7770b5f256f1c913f8e87ef
mass <amass1212@gmail.com>
2014-09-15 23:15:30 -0700

    Improve formatting of ShiftLights project.

--- Shift_Lights_Node.X/ShiftLights.h
+++ Shift_Lights_Node.X/ShiftLights.h
@@ -1,15 +1,11 @@
-/******************************************************************************
+/*
+ * @file: ShiftLights.h
  *
- *					Shift Lights Node C Main Code Header
- *
- ******************************************************************************
- * FileName:        ShiftLights.h
- * Dependencies:    none
- * Processor:       PIC18F46K80
- * Complier:        Microchip C18
- * Author:          Andrew Mass, George Schwieters
-*******************************************************************************/
-
+ * @processor: PIC18F46K80
+ * @compiler: Microchip C18
+ * @author: Andrew Mass
+ * @author: George Schwieters
+ */
 #ifndef MAIN_H
 #define MAIN_H
 
@@ -29,47 +25,47 @@
 #define ET_BYTE 0
 #define RPM_BYTE 0
 
-/*********************
- * Address Bytes     *
- *********************/
+/*****************
+ * Address Bytes *
+ *****************/
 
 #define TERM_LAT	LATCbits.LATC6
 
-/*********************
- * Data Bytes        *
- *********************/
+/**************
+ * Data Bytes *
+ **************/
 
 // Colors
-#define NONE                0b00000000
-#define RED                 0b01001000
-#define GREEN               0b00100100
-#define BLUE                0b00010010
-#define RGB                 0b01111110
-#define RED_GREEN           0b01101100
-#define RED_BLUE            0b01011010
-#define GREEN_BLUE          0b00110110
+#define NONE       0b00000000
+#define RED        0b01001000
+#define GREEN      0b00100100
+#define BLUE       0b00010010
+#define RGB        0b01111110
+#define RED_GREEN  0b01101100
+#define RED_BLUE   0b01011010
+#define GREEN_BLUE 0b00110110
 
 // Rev range
-#define REV_RANGE_1         7500
-#define REV_RANGE_2         8500
-#define REV_RANGE_3         9500
-#define REV_RANGE_4         10500
-#define REV_RANGE_5         11000
-#define REV_RANGE_6         11500
-#define REV_RANGE_7         12000
-#define REV_RANGE_8         12250
-#define REV_RANGE_LIMIT     12500
+#define REV_RANGE_1     7500
+#define REV_RANGE_2     8500
+#define REV_RANGE_3     9500
+#define REV_RANGE_4     10500
+#define REV_RANGE_5     11000
+#define REV_RANGE_6     11500
+#define REV_RANGE_7     12000
+#define REV_RANGE_8     12250
+#define REV_RANGE_LIMIT 12500
 
 // Custom color choices
-#define REV_COLOR           GREEN_BLUE
-#define REV_LIMIT_COLOR     GREEN
+#define REV_COLOR       GREEN_BLUE
+#define REV_LIMIT_COLOR GREEN
 
-/*********************
- * Method Headers    *
- *********************/
+/******************
+ * Method Headers *
+ ******************/
 
 void high_isr(void);
-void low_isr (void);
+void low_isr(void);
 void init_unused_pins(void);
 
 #endif
