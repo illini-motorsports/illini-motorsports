892c3551d6559d5f4397da49ac1c1fe6cdf30d16
bc8 <bc8@illinois.edu>
2020-03-02 07:44:25 -0600

    Initital WSB (Wheel Sensor Board) commit.

---
+++ WSB_Node.X/WSB.c
@@ -0,0 +1,23 @@
+/**
+ * WSB
+ *
+ * Processor:   PIC32MZ2048EFM100
+ * Compiler:    Microchip XC32
+ * Author:      Ben Cross
+ * Created:     2019-2020
+ */
+
+#include "WSB.h"
+
+void main(void)
+{
+  init_general();// Set general runtime configuration bits
+  init_gpio_pins();// Set all I/O pins to low outputs
+  init_oscillator(0);// Initialize oscillator configuration bits
+  init_timer2();// Initialize timer2 (millis)
+  init_adc(NULL); // Initialize ADC module
+  init_termination(NOT_TERMINATING);
+  init_can(); // Initialize CAN
+  STI();// Enable interrupts
+  
+}
---
+++ WSB_Node.X/WSB.h
@@ -0,0 +1,24 @@
+/**
+ * WSB Header
+ *
+ * Processor:   PIC32MZ2048EFM100
+ * Compiler:    Microchip XC32
+ * Author:      Ben Cross
+ * Created:     2019-2020
+ */
+
+#ifndef WSB_H
+#define	WSB_H
+
+#include <sys/types.h>
+#include "../FSAE.X/FSAE_config.h"
+#include "../FSAE.X/FSAE_can.h"
+#include "../FSAE.X/FSAE_adc.h"
+#include "../FSAE.X/CAN.h"
+#include "../FSAE.X/FSAE_spi.h"
+#include "../FSAE.X/FSAE_ad7490.h"
+#include "../FSAE.X/FSAE_max31855.h"
+
+
+#endif	/* WSB_H */
+
